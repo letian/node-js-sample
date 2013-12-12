237107b61ee65561066bcf8cc97e604470aa26d9
letian <kevin.letian@gmail.com>
2013-12-12 09:50:49 +0000

    testing change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 8088;
+var port = process.env.PORT || 443;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
